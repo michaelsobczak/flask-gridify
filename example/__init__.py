bff933040fec9fa8c0fb98dc4f10a3a4f9bbc484
Michael Sobczak <michaelsobczak54@gmail.com>
2020-02-18 08:31:24 -0500

    restructure example app to be more clear

---
+++ example/__init__.py
@@ -0,0 +1,29 @@
+import os
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+from flask_gridify import FlaskGridify
+
+from .models import User, Note, get_db_url, get_db_path
+
+app = Flask(__name__, template_folder='templates')
+app.config['SQLALCHEMY_DATABASE_URI'] = get_db_url()
+db = SQLAlchemy(app)
+
+# initialize the FlaskGridify extension
+#   the created pages will be /grids/<model_name> due to root_url_prefix
+grid = FlaskGridify(app, flask_sqlalchemy_db=db, root_url_prefix='/grids')
+
+@app.before_first_request
+def init_app():
+    if not os.path.exists(get_db_path()):
+        Base.metadata.create_all(bind=db.engine)
+
+    # register the User and Note SQLAlchemy models
+    #   This will create /grids/note and /grids/user pages with grids
+    grid.gridify(User)
+    grid.gridify(Note)
+
+
+from . import views, models
---
+++ example/models.py
@@ -0,0 +1,39 @@
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, ForeignKey, Integer, Text, create_engine, Table, Boolean, String
+from sqlalchemy.orm import relationship
+import os
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True)
+    username = Column(String(64), unique=True)
+    email = Column(String(120))
+    verified = Column(Boolean)
+    notes = relationship('Note')
+
+class Note(Base):
+    __tablename__ = 'note'
+    id = Column(Integer, primary_key=True)
+    title = Column(Text)
+    text = Column(Text)
+    user_id = Column(Integer, ForeignKey('user.id'))
+
+
+def get_db_path():
+    db_dir = os.path.join(
+        os.environ['HOME'],
+        '.local', 'share',
+        'flask-gridify-example'
+    )
+    if not os.path.exists(db_dir):
+        os.makedirs(db_dir)
+    db_url = os.path.join(db_dir, 'db.sqlite')
+    return db_url
+
+def get_db_url():
+    return f'sqlite:///{get_db_path()}'
+
+def get_engine():
+    return create_engine(get_db_url)
